b723b3086394d93331045076e24b339c7e72e8d8
Luciano Parintins Viana <luciano.viana@ufopa.edu.br>
2025-03-13 14:17:27 -0300

    controller: ProdutoControler

---
+++ app/src/main/java/com/filhotinhos/estoque/controller/ProdutoControler.java
@@ -0,0 +1,44 @@
+package com.filhotinhos.estoque.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.filhotinhos.estoque.model.Produto;
+import com.filhotinhos.estoque.service.ProdutoService;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoControler {
+    
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> listarTodos(){
+        return produtoService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.ok(produtoService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        return produtoService.salvar(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        produtoService.deletar(id);
+    }
+}
