b070430526a89fe231f110049ac7143d815516c9
Luciano Parintins Viana <luciano.viana@ufopa.edu.br>
2025-03-15 13:40:05 -0300

    refact: Refazendo as classes

--- app/src/main/java/com/filhotinhos/app/estoque/model/TipoMovimentacao.java
+++
@@ -1,6 +0,0 @@
-package com.filhotinhos.app.estoque.model;
-
-public enum TipoMovimentacao {
-    ENTRADA,
-    SAIDA
-}
--- app/src/main/java/com/filhotinhos/app/estoque/repository/ItemProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.filhotinhos.app.estoque.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.filhotinhos.app.estoque.model.ItemProduto;
-
-public interface ItemProdutoRepository extends JpaRepository<ItemProduto,Long> {
-    
-}
